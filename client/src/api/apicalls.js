281b2f79c2aae5b3fb7b6ab7e67e0840d262fb89
davyken <davykennang552@gmail.com>
2024-11-26 12:27:40 +0100

    updated colors

--- client/src/api/apicalls.js
+++ client/src/api/apicalls.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = `https://dchat-backend-1.onrender.com`;
+const API_URL = `https://localhost:5000`;
 const api = axios.create({
   baseURL: API_URL,
 });
