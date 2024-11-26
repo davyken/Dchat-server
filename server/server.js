5550c7ffce8a25beea170bdc373941a4f52345a1
davyken <davykennang552@gmail.com>
2024-11-26 12:21:12 +0100

    updated colors

--- server/server.js
+++ server/server.js
@@ -99,7 +99,7 @@ app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
 // Serve static files
-app.use(express.static(path.join(__dirname, "/client, dist")));
+app.use(express.static(path.join(__dirname, "/client/dist")));
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
